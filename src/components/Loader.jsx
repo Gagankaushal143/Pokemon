057b7f8bdb88330b006d239e20e20f740780bbbf
Gagan Kaushal <gagankaushal143@gmail.com>
2026-02-01 15:32:05 +0530

    Initial Commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <div className="h-10 w-10 rounded-full border-4 border-t-transparent border-blue-600 animate-spin"></div>
+    </div>
+  )
+}
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+export async function getPokemonList() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`)
+
+    const data = await res.json();
+    return data.results;
+}
